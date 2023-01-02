75ce25d7ff78e0e7be968e49d8860d0f3d0ce886
SauloRLM <saulo2000ramon@gmail.com>
2023-01-02 01:04:03 -0700

    listo

--- Rest-peticionesweb/src/controladores/sucursal.js
+++ Rest-peticionesweb/src/controladores/sucursal.js
@@ -76,11 +76,92 @@ function modificarSucursal(req,res){
                     //throw error;
                       res.status(200).send({Mensaje:'Error al modificar Empleado',Estatus:'Error'});
                     }else{                      
-                      if(params.estatus == 'B'){
-                        res.status(200).send({Mensaje:'Sucursal modificada con exito', Estatus:'Ok'});                  
+                      if(params.estatus == 'B'){                                                                        
                         //obtener los datos de almacen y cargarlos a la 1 como disponibles, por ultimo borrarlos de almacen.
+                        var query = connection.query('SELECT id_codigo_articulo,cantidad_total,tipo FROM almacen WHERE id_sucursal = ?', [id_sucursal], function(error, result){
+                          if(error){
+                            // throw error;
+                            res.status(200).send({Mensaje:'Error en la petición',Estatus:'Error'});
+                          }else{
+                      
+                            var stockSucursal = result;                                  
+                            var banderaError = 'a'
+                            if(stockSucursal.length != 0){
+                              
+                              //console.log(stockSucursal);
 
+                              //hacer un foreach para preguntar si existen en el almacen y sino para cargarlos 
+                              stockSucursal.forEach(element => {
 
+                                //element.id_codigo_articulo
+                                //element.cantidad_total
+                                //element.tipo
+                                //console.log(element.cantidad_total);
+
+                                //preguntar si existe el articulo en el almacen 1 si es asi sumarle
+                                var query_exist_almacen = connection.query('SELECT cantidad_disponible from almacen  where id_sucursal = ? and id_codigo_articulo = ?',
+                                  [1,element.id_codigo_articulo],function(error, result){
+                                  if(error){
+                                    //si hubo error al preguntar                                    
+                                    banderaError = 'b';
+                                  }else{
+                                    var disponible = result;  
+                                    //console.log(disponible.length);
+                                    if(disponible.length != 0){                                                
+                                      //si existe  sumarlo 
+                                      //console.log('Cantidad de alamacen: '+result[0].cantidad_disponible)
+                                      var cantidadAlmacen = 0;                                      
+                                      cantidadAlmacen = result[0].cantidad_disponible + element.cantidad_total;                                      
+                                      var query_almacen = connection.query('UPDATE almacen SET cantidad_disponible = ? WHERE id_sucursal = ? and id_codigo_articulo = ?;',
+                                      [cantidadAlmacen,1,element.id_codigo_articulo],function(error, result){
+                                        if(error){
+                                        //throw error;                                          
+                                          banderaError = 'b';
+                                        }else{                                                 
+                                          //Borrar dicho almacen por id_sucursal y por codigo
+                                          var query_almacen = connection.query('DELETE FROM almacen WHERE id_sucursal = ? AND id_codigo_articulo = ?;',
+                                          [ id_sucursal,element.id_codigo_articulo],function(error, result){
+                                          if(error){
+                                            //throw error;
+                                            banderaError = 'b';                                            
+                                          }
+                                          });                                                                                                            
+                                        }
+                                      });                                                                                                            
+                                    }else{                                      
+                                      //sino existe crearlo y eliminar el almacen
+                                      var query = connection.query('INSERT INTO almacen(id_sucursal, id_codigo_articulo, cantidad_total, cantidad_disponible, tipo) VALUES(?,?,?,?,?)',
+                                      [1,element.id_codigo_articulo,0,element.cantidad_total,element.tipo],function(error, result){
+                                        if(error){
+                                        // throw error;                                        
+                                          banderaError = 'b';
+                                        }else{
+                                          //Borrar dicho almacen por id_sucursal y por codigo
+                                          var query_almacen = connection.query('DELETE FROM almacen WHERE id_sucursal = ? AND id_codigo_articulo = ?;',
+                                          [ id_sucursal,element.id_codigo_articulo],function(error, result){
+                                          if(error){
+                                            //throw error;                                            
+                                            banderaError = 'b';
+                                          }
+                                          });                                                
+                                        }
+                                      });                                                                                
+                                    }
+                                  }
+                                });                                                  
+                              });
+
+                              if(banderaError == 'b'){
+                                res.status(200).send({Mensaje:'Error. al cargar stock a almacen', Estatus:'Error'});
+                              }else{
+                                res.status(200).send({Mensaje:'Sucursal modificada con exito', Estatus:'Ok'});
+                              }
+
+                            }else{
+                              res.status(200).send({Mensaje:'Sucursal modificada con exito', Estatus:'Ok'});
+                            }
+                          }
+                        });
 
                       }else{                        
                         res.status(200).send({Mensaje:'Sucursal modificada con exito', Estatus:'Ok'});
